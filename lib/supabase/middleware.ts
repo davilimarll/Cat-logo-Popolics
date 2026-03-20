7e9e636b8b20cff1bdfabbbb83cebc48b77d5cf2
davilimarll <davilimarl@gmail.com>
2026-03-20 18:59:36 -0300

    Update middleware.ts

--- lib/supabase/middleware.ts
+++ lib/supabase/middleware.ts
@@ -2,7 +2,6 @@ import { createServerClient } from "@supabase/ssr";
 import { NextResponse, type NextRequest } from "next/server";
 
 export async function updateSession(request: NextRequest) {
-  const response = NextResponse.next({ request });
   let response = NextResponse.next({ request });
 
   const supabase = createServerClient(
